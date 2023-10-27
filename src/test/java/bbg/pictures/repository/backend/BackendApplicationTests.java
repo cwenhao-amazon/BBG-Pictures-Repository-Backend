e25db3d9c2793fae960f2488056bb23ef3363d49
molgergo01 <mol.gergo01@gmail.com>
2023-10-27 22:59:14 +0200

    Init repo

---
+++ src/test/java/bbg/pictures/repository/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bbg.pictures.repository.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
